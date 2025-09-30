0b8e73b4c2c7d99b6a425f693e180da5af320f15
mahin-git <mahindeen1999@gmail.com>
2025-09-30 16:06:50 +0530

    Initial commit

---
+++ Readme Login Details.txt
@@ -0,0 +1,8 @@
+Admin Login Credentials 
+* Email - admin@mail.com
+* Password - Password@123
+
+Doctor login Credentials
+
+* ID - YDS7L
+* Password - password
---
+++ backend/doc/assets/inc/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                2025 - <?php echo date('Y'); ?> &copy; Hospital Management Information System. </a>
+            </div>
+
+        </div>
+    </div>
+</footer>
